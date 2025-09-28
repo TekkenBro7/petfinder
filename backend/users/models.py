3002d97b76ffc9ed4c1b3ca958c12b0250294bc1
Maxim <maks.kreet@mail.ru>
2025-09-28 15:37:10 +0300

    Create ads app and add phone number validator

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,5 +1,10 @@
+from typing import Any
+
+import phonenumbers
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+from users.validators import validate_phone
 
 
 class User(AbstractUser):
@@ -9,13 +14,24 @@ class User(AbstractUser):
     )
 
     role = models.CharField(max_length=10, choices=ROLE_CHOICES, default="user")
-    phone = models.CharField(max_length=15, blank=True, default="", unique=True)
-    email = models.EmailField(unique=True, blank=True)
+    phone = models.CharField(
+        max_length=15, blank=True, default="", unique=True, validators=[validate_phone]
+    )
+    email = models.EmailField(unique=True, blank=True, null=True) # type: ignore
 
     class Meta:
         verbose_name = "User"
         verbose_name_plural = "Users"
         ordering = ["-date_joined"]
+
+    def save(self, *args: Any, **kwargs: Any) -> None:
+        if self.phone:
+            try:
+                number = phonenumbers.parse(self.phone, None)
+                self.phone = phonenumbers.format_number(number, phonenumbers.PhoneNumberFormat.E164)
+            except phonenumbers.NumberParseException:
+                pass
+        super().save(*args, **kwargs)
 
     def __str__(self) -> str:
         return self.username
---
+++ backend/users/validators.py
@@ -0,0 +1,18 @@
+import re
+
+import phonenumbers
+from django.core.exceptions import ValidationError
+
+
+def validate_phone(value: str) -> None:
+    if not re.fullmatch(r"[+\d\s\-\(\)]+", value):
+        raise ValidationError("The phone number contains invalid characters.")
+
+    try:
+        number = phonenumbers.parse(value, None)
+        if not phonenumbers.is_valid_number(number):
+            raise ValidationError("Enter the correct phone number.")
+        if phonenumbers.region_code_for_number(number) not in ["RU", "BY"]:
+            raise ValidationError("Only Russian and Belarusian numbers are supported..")
+    except phonenumbers.NumberParseException:
+        raise ValidationError("Incorrect phone number format.")
